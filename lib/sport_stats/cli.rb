515b632d554417675abfcc5bc8a564b85b37ac05
tylertaylor <vikingtyty@gmail.com>
2016-08-10 08:52:07 -0500

    built up interface

--- lib/sport_stats/cli.rb
+++ lib/sport_stats/cli.rb
@@ -1,11 +1,38 @@
 class SportStats::CLI
 
   def call
-    puts "Sport Stats:"
+    puts "Sport Stats: \n"
+    list_sports
+    menu
   end
 
-  def print
+  def list_sports
+    puts "Which league would you like to view?"
+    puts """
+      1. NBA
+      2. NFL
+      3. MLB
+    """
+  end
 
+  def menu
+    input = nil
+    while input != "exit"
+      puts "Enter the number or name of the league you'd like more info on:"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "More info on the NBA..."
+      when "2"
+        puts "More info on the NFL..."
+      when "3"
+        puts "More info on MLB..."
+      when "list"
+        list_sports
+      else
+        puts "I don't recognize that command. Please type list or exit."
+      end
+    end
   end
 
 end
